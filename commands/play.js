754f2d9f56fee6757fad6bbbcb3b17cadd0c8ae3
Ruimtevaart <granata0520@gmail.com>
2023-01-18 13:16:15 +0200

    Broken queue play

--- commands/play.js
+++ commands/play.js
@@ -16,31 +16,51 @@ module.exports = {
         // if (connection == null) {
         
         // const connection = null;
+        var requestArray = null;
+        // var resource = null;
+        // var subscription = null;
+        // var player = null;
         let connection = getVoiceConnection(interaction.guild.id);
-        const fileName = dir + interaction.guild.id + '-' + interaction.member.voice.channelId + '.json';
+        const fileName = dir + interaction.guild.id + '.json';
         // let play = get
-        if (connection == null && !fs.existsSync(fileName)) {
-            console.log("triggered");
+        // if (connection == null && !fs.existsSync(fileName)) {
+        if (connection == null) {
+            // console.log("triggered");
             // var requestArray = [];
-            // connection = joinVoiceChannel({
-            //     channelId: interaction.member.voice.channelId,
-            //     guildId: interaction.guild.id,
-            //     adapterCreator: interaction.guild.voiceAdapterCreator,
-            // });
+            connection = joinVoiceChannel({
+                channelId: interaction.member.voice.channelId,
+                guildId: interaction.guild.id,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
             var requestFile = fs.openSync(fileName, 'w');
-            console.log(requestFile);
-            await interaction.reply({ content: fileName, ephemeral: true });
-            return;
+            fs.writeFile(fileName, JSON.stringify([]), (error) => {if (error) console.log(error);});
+            // console.log(requestFile);
+            try {
+                await entersState(connection, VoiceConnectionStatus.Ready, 5000);
+                console.log("Connected: " + interaction.member.voice.channel.name);
+                await interaction.reply('Playing: ' + interaction.options.getString('url'));
+                // await sleep(5000);
+            } catch (error) {
+                console.log("Voice Connection not ready within 5s.", error);
+                return null;    
+            }
+            const player = createAudioPlayer();
+            console.log(interaction.options.getString('url'));
+            const stream = await playdl.stream(interaction.options.getString('url'));
+            const resource = createAudioResource(stream.stream, { inputType: stream.type });
+            player.play(resource);
+            // await interaction.reply({ content: fileName, ephemeral: true });
+            // return;
         } else {
             // requestArray.push(interaction.options.getString('url'));
-            let requestArray = JSON.parse(fs.readFileSync(fileName));
-            await interaction.reply({ content: JSON.stringify(requestArray), ephemeral: true });
-            return;
+            requestArray = JSON.parse(fs.readFileSync(fileName));
+            // await interaction.reply({ content: JSON.stringify(requestArray), ephemeral: true });
+            // return;
         }
 
-        const player = createAudioPlayer();
-        console.log(interaction.options.getString('url'));
-        const stream = await playdl.stream(interaction.options.getString('url'));
+        // const player = createAudioPlayer();
+        // console.log(interaction.options.getString('url'));
+        // const stream = await playdl.stream(interaction.options.getString('url'));
         
         connection.on('stateChange', (oldState, newState) => {
             console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
@@ -50,28 +70,36 @@ module.exports = {
         });
 
         // console.log(stream);
-        const resource = createAudioResource(stream.stream, { inputType: stream.type });
-        const subscription = connection.subscribe(player);
+        // const resource = createAudioResource(stream.stream, { inputType: stream.type });
+        // const subscription = connection.subscribe(player);
 
 
-        try {
-            await entersState(connection, VoiceConnectionStatus.Ready, 5000);
-            console.log("Connected: " + interaction.member.voice.channel.name);
-            await interaction.reply('Playing: ' + interaction.options.getString('url'));
-            // await sleep(5000);
-        } catch (error) {
-            console.log("Voice Connection not ready within 5s.", error);
-            return null;    
-        }
+        // try {
+        //     await entersState(connection, VoiceConnectionStatus.Ready, 5000);
+        //     console.log("Connected: " + interaction.member.voice.channel.name);
+        //     await interaction.reply('Playing: ' + interaction.options.getString('url'));
+        //     // await sleep(5000);
+        // } catch (error) {
+        //     console.log("Voice Connection not ready within 5s.", error);
+        //     return null;    
+        // }
 
         
         
-        player.play(resource);
+        // player.play(resource);
 
         player.on(AudioPlayerStatus.Idle, () => {
             // console.log(requestArray);
-            player.stop();
-            connection.destroy();
+            requestArray = JSON.parse(fs.readFileSync(fileName));
+            if (requestArray == []) {
+                player.stop();
+                connection.destroy();
+            } else {
+                const stream = playdl.stream(interaction.options.getString('url'));
+                const resource = createAudioResource(stream.stream, { inputType: stream.type });
+                player.play(resource);
+                fs.writeFile(fileName, JSON.stringify({}), (error) => {if (error) console.log(error);});
+            }
         });
 
         player.on('error', error => {
