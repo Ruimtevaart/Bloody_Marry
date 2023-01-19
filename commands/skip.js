44b00f4f478da4787bad0bfce3a9a9704a92667b
Ruimtevaart <granata0520@gmail.com>
2023-01-19 22:04:30 +0200

    First skip commit, broken, needs deploy

---
+++ commands/skip.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, VoiceConnectionStatus, getVoiceConnection, AudioPlayerStatus } = require('@discordjs/voice');
+const playdl = require('play-dl');
+const fs = require('fs');
+const dir = './songRequests/';
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('skip').setDescription('Skip currently plating song'),
+    async execute(interaction) {
+        if (interaction.member.voice.channelId == null) {
+            await interaction.reply('You need to join a voice channel first! Bozo');
+            return;
+        }
+        const connection = getVoiceConnection(interaction.guild.id);
+        if (connection == null) {
+            await interaction.reply('The bot is not currently playing anything');
+            return;
+        }
+        
+    },
+};
